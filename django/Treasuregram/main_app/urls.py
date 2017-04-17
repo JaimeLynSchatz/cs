ae749634892640569b94429250ae5e956181e13e
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2017-04-17 12:24:54 -0700

    continuing with django'

---
+++ django/Treasuregram/main_app/urls.py
@@ -0,0 +1,12 @@
+# missing some import statements here
+
+# url list
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index),
+	url(r'^([0-9]+)/$', views.detail, name = 'detail'),
+	url(r'^post_url/$', views.post_treasure, name='post_treasure'),
+]
--- django/Treasuregram/main_app/views.py
+++ django/Treasuregram/main_app/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .models import Treasure
+from .forms import TreasureForm
 
 # Create your views here.
 def index(request):
-	return HttpResponse('<h1>Hello Explorers!</h1>')
+	treasures = Treasure.objects.all()
+	form = TreasureForm()
+	return render(request, 'index.html', {'treasures' : treasures, 'form' : form})
 
 def home(request):
-	return HttpResponse('<h1>Welcome Home, Travellers!</h1>')
+	return render(request, 'home.html')
+
+def detail(request, treasure_id):
+	treasure = Treasure.objects.get(id=treasure_id)
+	return render(request, 'detail.html', {'treasure': treasure})
+
+def post_treasure(request):
+	form = TreasureForm(request.POST)
+	if form.is_valid():
+		treasure = Treasure(name = form.cleaned_data['name'],
+							value = form.cleaned_data['value'],
+							material = form.cleaned_data['material'],
+							location = form.cleaned_data['location'],
+							img_url = form.cleaned_data['img_url'])
+	treasure.save()
+	return HttpResponseRedirect('/')
